f80114a216295dbeadd9769d05153f34b0ab5096
ShubhamSharma20007 <shubhamsharma20007@gmail.com>
2026-09-19 11:55:11 +0530

    fix

--- src/services/agent/src/utils/logger.js
+++ src/services/agent/src/utils/logger.js
@@ -1,15 +1,16 @@
 import pino from "pino";
-import PinoPretty from "pino-pretty";
+
 const logger = pino({
-    level:'info',
+    level: "info",
+
     transport: {
-        target: 'pino-pretty',
+        target: "pino-pretty",
         options: {
             colorize: true,
-            translateTime: 'SYS:yyyy-mm-dd HH:MM:ss',
-            ignore: 'pid,hostname'
+            translateTime: "SYS:yyyy-mm-dd HH:MM:ss",
+            ignore: "pid,hostname"
         }
     }
+});
 
-})
 export default logger;
