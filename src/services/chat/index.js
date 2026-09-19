485ee3f04417339248dda40181eb942337bd6aee
ShubhamSharma20007 <shubhamsharma20007@gmail.com>
2026-09-19 15:21:47 +0530

    add PATCH Method

--- src/services/chat/index.js
+++ src/services/chat/index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ limit: '50mb',extended: true }));
 
 app.use(cors({
     origin: process.env.CLIENT_URL,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     credentials: true,
   }));
   
